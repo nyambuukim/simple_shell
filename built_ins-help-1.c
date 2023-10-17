1f9e079c5ede8eabf5386ef8a4c37b435dbe7c8e
JayJust0h <edwinjustus11@gmail.com>
2023-10-17 09:57:45 -0700

    built_ins-help-1.c

--- built_ins-help-1.c
+++ built_ins-help-1.c
@@ -1,21 +1,25 @@
 #include "shell.h"
-void help_all(void);
+
 void help_alias(void);
 void help_cd(void);
-void help_exit(void);
 void help_help(void);
+void help_all(void);
+void help_exit(void);
+
 
 /**
-* help_all - Displays all possible builtin shellby commands.
+* help_all - Builtin shellby commands.
 */
+
 void help_all(void)
+
 {
-char *msg = "Shellby\nThese shell commands are defined internally.\n";
+	char *msg = "Shellby\nCommands already defined.\n";
 
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "Type 'help' to see this list.\nType 'help name' to find ";
+msg = "Type 'help' to view.\nType 'help_name' to specify ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "out more about the function 'name'.\n\n  alias   \t";
+msg = "the function 'name'.\n\n  alias   \t";
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
 write(STDOUT_FILENO, msg, _strlen(msg));
@@ -28,74 +32,79 @@ write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
-* help_alias - Displays information on the shellby builtin command 'alias'.
+* help_alias - Shellby builtin 'alias'.
 */
+
 void help_alias(void)
+
 {
-char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
+char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandle all aliases.\n";
 
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "\n\talias: Prints a list of all aliases, one per line, in ";
+msg = "\n\talias: Print list aliases, Single line, in ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
+msg = "the format NAME='VALUE'.\n\talias_name [name-2 ...]:print";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = " the aliases name, name2, etc. one per line, in the ";
+msg = " alias name, name-2, etc. one_line, in ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
+msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Define";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = " an alias for each NAME whose VALUE is given. If NAME ";
+msg = " alias for each NAME where VALUE is prsnt. If NAME ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "is already an alias, replace its value with VALUE.\n";
+msg = "alias exist, replace with VALUE.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
-* help_cd - Displays information on the shellby builtin command 'cd'.
+* help_cd - Shellby builtin command 'cd'.
 */
 void help_cd(void)
 {
-char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
+char *msg = "cd: cd [DIRECTORY]\n\tChange the current dir of ";
 
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
+msg = " process to DIR.\n\n\tIf no argument, the ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "command is interpreted as cd $HOME. If the argument '-' is";
+msg = "command cd $HOME. If argument '-' is";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = " given, the command is interpreted as cd $OLDPWD.\n\n";
+msg = " present, the command cd $OLDPWD.\n\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "\tThe environment variables PWD and OLDPWD are updated ";
+msg = "\tThe env var PWD & OLDPWD are update ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "after a change of directory.\n";
+msg = "change of dir.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
-* help_exit - Displays information on the shellby builtin command 'exit'.
+* help_exit - Shellby builtin command 'exit'.
 */
+
 void help_exit(void)
+
 {
-char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
+	char *msg = "exit: exit [STATUS]\n\tExit shell.\n\n\tThe ";
 
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "STATUS argument is the integer used to exit the shell.";
+msg = "STATUS argument is the int to exit shell.";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = " If no argument is given, the command is interpreted as";
+msg = " No argument, the command is ";
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = " exit 0.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
-* help_help - Displays information on the shellby builtin command 'help'.
+* help_help - Shellby builtin command 'help'.
 */
-void help_help(void)
+
+	void help_help(void)
 {
-char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
+char *msg = "help: help\n\tShellby builtin commands.\n";
 
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
+msg = "\n      help [BUILTIN NAME]\n\tSee each ";
 write(STDOUT_FILENO, msg, _strlen(msg));
-msg = "builtin command.\n";
+msg = "builtin command info.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
 

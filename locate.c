a4c6dc476ca2ea19b1a414f5c02fc4b3144cc9f0
nyambuukim <nyambuukim1@gmail.com>
2023-10-17 03:16:40 -0700

    locate.c

--- locate.c
+++ locate.c
@@ -4,137 +4,135 @@ char *fill_path_dir(char *path);
 list_t *get_path_dir(char *path);
 
 /**
- * get_location - Locates a command in the PATH.
- * @command: The command to locate.
- *
- * Return: If an error occurs or the command cannot be located - NULL.
- *         Otherwise - the full pathname of the command.
+ * get_location - Loc of command_path.
+ * @command: Command.
+ * Return: Pathname/Null(Error).
  */
+
 char *get_location(char *command)
+
 {
-        char **path, *temp;
-        list_t *dirs, *head;
-        struct stat st;
+	char **path, *tmp;
+	list_t *dirs, *head;
+	struct stat stir;
 
-        path = _getenv("PATH");
-        if (!path || !(*path))
-                return (NULL);
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
 
-        dirs = get_path_dir(*path + 5);
-        head = dirs;
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
 
-        while (dirs)
-        {
-                temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
-                if (!temp)
-                        return (NULL);
+	while (dirs)
+	{
+		tmp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!tmp)
+			return (NULL);
 
-                _strcpy(temp, dirs->dir);
-                _strcat(temp, "/");
-                _strcat(temp, command);
+		_strcpy(tmp, dirs->dir);
+		_strcat(tmp, "/");
+		_strcat(tmp, command);
 
-                if (stat(temp, &st) == 0)
-                {
-                        free_list(head);
-                        return (temp);
-                }
+		if (stat(tmp, &stir) == 0)
+		{
+			free_list(head);
+			return (tmp);
+		}
 
-                dirs = dirs->next;
-                free(temp);
-        }
+		dirs = dirs->next;
+		free(tmp);
+	}
 
-        free_list(head);
+	free_list(head);
 
-        return (NULL);
+	return (NULL);
 }
 
 /**
- * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
- *                 colons (:) with current working directory.
- * @path: The colon-separated list of directories.
- *
- * Return: A copy of path with any leading/sandwiched/trailing colons replaced
- *         with the current working directory.
+ * fill_path_dir - Copies PATH.
+ * @path: The : of directories.
+ * Return: Path copy.
  */
+
 char *fill_path_dir(char *path)
-{
-        int i, length = 0;
-        char *path_copy, *pwd;
 
-        pwd = *(_getenv("PWD")) + 4;
-        for (i = 0; path[i]; i++)
-        {
-                if (path[i] == ':')
-                {
-                        if (path[i + 1] == ':' || i == 0 || path[i + 1] == '\0')
-                                length += _strlen(pwd) + 1;
-                        else
-                                length++;
-                }
-                else
-                        length++;
-        }
-        path_copy = malloc(sizeof(char) * (length + 1));
-        if (!path_copy)
-                return (NULL);
-        path_copy[0] = '\0';
-        for (i = 0; path[i]; i++)
-        {
-                if (path[i] == ':')
-                {
-                        if (i == 0)
-                        {
-                                _strcat(path_copy, pwd);
-                                _strcat(path_copy, ":");
-                        }
-                        else if (path[i + 1] == ':' || path[i + 1] == '\0')
-                        {
-                                _strcat(path_copy, ":");
-                                _strcat(path_copy, pwd);
-                        }
-                        else
-                                _strcat(path_copy, ":");
-                }
-                else
-                {
-                        _strncat(path_copy, &path[i], 1);
-                }
-        }
-        return (path_copy);
+{
+	int is, len = 0;
+	char *path_copy, *pwkd;
+
+	pwkd = *(_getenv("PWD")) + 4;
+	for (is = 0; path[is]; is++)
+	{
+		if (path[is] == ':')
+		{
+			if (path[is + 1] == ':' || is == 0 || path[is + 1] == '\0')
+				len += _strlen(pwkd) + 1;
+			else
+				len++;
+		}
+		else
+			len++;
+	}
+	path_copy = malloc(sizeof(char) * (len + 1));
+	if (!path_copy)
+		return (NULL);
+	path_copy[0] = '\0';
+	for (is = 0; path[is]; is++)
+	{
+		if (path[is] == ':')
+		{
+			if (is == 0)
+			{
+				_strcat(path_copy, pwkd);
+				_strcat(path_copy, ":");
+			}
+			else if (path[is + 1] == ':' || path[is + 1] == '\0')
+			{
+				_strcat(path_copy, ":");
+				_strcat(path_copy, pwkd);	
+			}
+			else
+				_strcat(path_copy, ":");
+		}
+		else
+		{
+		_strncat(path_copy, &path[is], 1);
+		}
+	}
+	return (path_copy);
 }
 
 /**
- * get_path_dir - Tokenizes a colon-separated list of
- *                directories into a list_s linked list.
-* @path: The colon-separated list of directories.
- *
-* Return: A pointer to the initialized linked list.
+ * get_path_dir - Tokenizer.
+ * @path:on-separated list of directories
+* Return: A pointer.
  */
+
 list_t *get_path_dir(char *path)
 {
-        int index;
-        char **dirs, *path_copy;
-        list_t *head = NULL;
+	int indx;
+	char **dirst, *path_copy;
+	list_t *head = NULL;
 
-        path_copy = fill_path_dir(path);
-        if (!path_copy)
-                return (NULL);
-        dirs = _strtok(path_copy, ":");
-        free(path_copy);
-        if (!dirs)
-                return (NULL);
+	path_copy = fill_path_dir(path);
+	if (!path_copy)
+		return (NULL);
+	dirst = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirst)
+		return (NULL);
 
-        for (index = 0; dirs[index]; index++)
-        {
-                if (add_node_end(&head, dirs[index]) == NULL)
-                {
-                        free_list(head);
-                        free(dirs);
-                        return (NULL);
-                }
-        }
+	for (indx = 0; dirst[indx]; indx++)
+	{
+		if (add_node_end(&head, dirst[indx]) == NULL)
+		{
+			free_list(head);
+			free(dirst);
+			return (NULL);
+		}
+	}
 
-        free(dirs);
+	free(dirst);
 
-        return (head);
+	return (head);
 }

87a45f5ce03e69177d7ce82c6c6df2cff0b72127
JayJust0h <edwinjustus11@gmail.com>
2023-10-17 08:25:27 -0700

    errors.c

--- errors.c
+++ errors.c
@@ -1,114 +1,117 @@
 #include "shell.h"
+
 int num_len(int num);
-char *_itoa(int num);
 int create_error(char **args, int err);
+char *_itoa(int num);
 
 /**
-* num_len - Counts the digit length of a number.
-* @num: The number to measure.
-*
-* Return: The digit length.
+* num_len - digit length.
+* @num: Number.
+* Return:length.
 */
+
 int num_len(int num)
-{
-unsigned int num1;
-int len = 1;
 
-if (num < 0)
 {
-len++;
-num1 = num * -1;
-}
-else
-{
-num1 = num;
-}
-while (num1 > 9)
-{
-len++;
-num1 /= 10;
-}
+	unsigned int num1;
+	int len = 1;
 
-return (len);
+	if (num < 0)
+		{
+			len++;
+			num1 = num * -1;
+		}
+	else
+	{
+		num1 = num;
+	}
+	while (num1 > 9)
+	{
+		len++;
+		num1 /= 10;
+	}
+
+	return (len);
 }
 
 /**
-* _itoa - Converts an integer to a string.
-* @num: The integer.
-*
-* Return: The converted string.
+* _itoa - Integer to string.
+* @num: Integer.
+* Return: converted string.
 */
+
 char *_itoa(int num)
+
 {
-char *buffer;
-int len = num_len(num);
-unsigned int num1;
+	char *buff;
+	int len = num_len(num);
+	unsigned int num1;
 
-buffer = malloc(sizeof(char) * (len + 1));
-if (!buffer)
-return (NULL);
+	buff = malloc(sizeof(char) * (len + 1));
+	if (!buff)
+	return (NULL);
 
-buffer[len] = '\0';
+	buff[len] = '\0';
 
-if (num < 0)
-{
-num1 = num * -1;
-buffer[0] = '-';
-}
-else
-{
-num1 = num;
-}
+	if (num < 0)
+	{
+		num1 = num * -1;
+		buff[0] = '-';
+	}
+	else
+	{
+		num1 = num;
+	}
 
-len--;
-do {
-buffer[len] = (num1 % 10) + '0';
-num1 /= 10;
-len--;
-} while (num1 > 0);
+		len--;
+	do {
+		buff[len] = (num1 % 10) + '0';
+		num1 /= 10;
+		len--;
+	} while (num1 > 0);
 
-return (buffer);
+	return (buff);
 }
 
 /**
-* create_error - Writes a custom error message to stderr.
-* @args: An array of arguments.
-* @err: The error value.
-*
+* create_error - custom error message stderr.
+* @args: Arguments.
+* @err: Error value.
 * Return: The error value.
 */
+
 int create_error(char **args, int err)
 {
-char *error;
+	char *error;
 
-switch (err)
-{
-case -1:
-error = error_env(args);
-break;
-case 1:
-error = error_1(args);
-break;
-case 2:
-if (*(args[0]) == 'e')
-error = error_2_exit(++args);
-else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
-error = error_2_syntax(args);
-else
-error = error_2_cd(args);
-break;
-case 126:
-error = error_126(args);
-break;
-case 127:
-error = error_127(args);
-break;
-}
-write(STDERR_FILENO, error, _strlen(error));
+	switch (err)
+	{
+		case -1:
+		error = error_env(args);
+		break;
+		case 1:
+		error = error_1(args);
+		break;
+		case 2:
+	if (*(args[0]) == 'e')
+		error = error_2_exit(++args);
+	else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+		error = error_2_syntax(args);
+	else
+		error = error_2_cd(args);
+	break;
+	case 126:
+		error = error_126(args);
+	break;
+	case 127:
+		error = error_127(args);
+	break;
+	}
+		write(STDERR_FILENO, error, _strlen(error));
 
-if (error)
-free(error);
-return (err);
+	if (error)
+	free(error);
+	return (err);
 
 }
 
